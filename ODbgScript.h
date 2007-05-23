4d17c299b499db7e06ec6e89272f254942d81fed
epsylon3 <epsylon3@gmail.com>
2007-05-23 08:33:51 +0000

    Release 1.61

--- ODbgScript.h
+++ ODbgScript.h
@@ -4,7 +4,7 @@ using namespace std;
 
 #define VERSIONHI      1               // High plugin version
 #define VERSIONLO      61              // Low plugin version
-#define VERSIONST      1               // plugin state (1svn 2beta 3final)
+#define VERSIONST      3               // plugin state (1svn 2beta 3final)
 
 ///long
 static char      wndprogclass[32];	   // Name of progress window class
